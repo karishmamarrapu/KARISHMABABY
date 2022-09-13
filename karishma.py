6b32808144d4f9864833c82b01b328fbea7e4368
karishmamarrapu <113088990+karishmamarrapu@users.noreply.github.com>
2022-09-13 17:16:07 +0530

    Add files via upload

---
+++ karishma.py
@@ -0,0 +1,32 @@
+first_name=input("enter the first name : ")
+last_name =input("enter the last name : ")
+date_of_birth=input("enter the date of birth : ")
+mobile_number=input("enter the mobile number : ")
+higher_education =input("enter the education details : ")
+hobies=input("enter the hobies: ")
+email=("enter email ID")
+ssc=input("enter the school name : ")
+inter=("srichaitanya junior college")
+
+
+print("_____BIO DATA____")
+print(f"first name       : {first_name}")
+print(f"LAST NAME         : {last_name}")
+print(f"DATE OF BIRTH     : {date_of_birth}")
+print(f"MOBILE NUMBER     : {mobile_number}")
+print(f"HIGHER_EDUCATION  : {higher_education}")
+print(f"HOBIES            : {hobies}")
+print(f"email ID         :  {email}")
+
+
+
+
+
+
+
+
+
+
+
+
+
